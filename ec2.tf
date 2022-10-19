18c5de3b2c531c70b39a0236bea1ac0133d228ba
eragam2295 <91144349+eragam2295@users.noreply.github.com>
2022-10-19 13:16:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,7 @@
 resource "aws_instance" "ter_instance1" {
   ami           = "ami-0c6a6b0e75b2b6ce7"
    instance_type = "t2.micro"
-   subnet_id = aws_subnet.terrsubnet.id
    key_name = "lol"
-   vpc_security_group_ids = [aws_security_group.terr-security.id]
    associate_public_ip_address= "true"
 count = "2"
   tags = {
